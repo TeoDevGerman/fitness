ee25f4e2cafc2849b2f7ccc72f39d63ee750b958
Teoman Nejat Wiedenbeck <t.wiedenbeck.1@campus.tu-berlin.de>
2021-01-10 15:46:16 +0100

    Der commit des Jahres

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
+// das ist ein test
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
